bf79db257b76f70b03b076ec06b55aee68aa97b0
Vedran V <drvicko@hotmail.com>
2022-03-31 13:22:12 +0200

    JS Remainder %

---
+++ ex07/src/index.js
@@ -0,0 +1,14 @@
+function main (numOne, numTwo) 
+{
+    
+
+    // Only change code below this line
+    var remainder = numOne % numTwo; 
+
+
+    // Only change code above this line
+    
+    return remainder; 
+}
+console.log(main(19, 2));
+module.exports = main;
