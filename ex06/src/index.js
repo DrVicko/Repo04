13b731a6d761ff4531f2c4e60dec2fcb8fff9374
Vedran V <drvicko@hotmail.com>
2022-03-31 13:11:16 +0200

    Dividing floats

---
+++ ex06/src/index.js
@@ -0,0 +1,14 @@
+function main (numOne, numTwo) 
+{
+    
+
+    // Only change code below this line
+    var myQuotient = numOne / numTwo; 
+
+
+    // Only change code above this line
+    
+    return myQuotient; 
+}
+console.log(main(16.5, 5.5));
+module.exports = main;
